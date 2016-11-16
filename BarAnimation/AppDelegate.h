4775ed75afb0cd3a8f051986ac96a50d477178e6
zwz <zhaoweizheng66@163.com>
2016-11-16 19:46:38 +0800

    Initial Commit

---
+++ BarAnimation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BarAnimation
+//
+//  Created by zwz on 2016/11/16.
+//  Copyright © 2016年 赵伟争. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
